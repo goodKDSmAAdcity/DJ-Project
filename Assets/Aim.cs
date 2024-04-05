f798a4337d08a8557af436676ac48774da6708b1
goodKDSmAAdcity <128930159+goodKDSmAAdcity@users.noreply.github.com>
2024-04-06 01:28:14 +0300

    added battel

---
+++ Assets/Aim.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+    private float timer;
+    private GameObject player;
+    void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        timer += Time.deltaTime;
+        if(timer > 2)
+        {
+            timer = 0;
+            Shoot();
+        }
+        Vector3 direction = player.transform.position - transform.position;
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, bulletPos.position,bulletPos.rotation);
+    }
+}
