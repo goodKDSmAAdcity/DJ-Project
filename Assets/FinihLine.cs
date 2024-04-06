a0b5b84a3665dc5ff3b7bdd076f5bb9728eb0a13
SebiTheyThem <166058726+SebiTheyThem@users.noreply.github.com>
2024-04-06 21:45:12 +0300

    LevelRace

--- Assets/FinihLine.cs
+++ Assets/FinihLine.cs
@@ -5,7 +5,7 @@ public class FinihLine : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("FinishLine"))
+        if (collision.gameObject.CompareTag("Finish"))
         {
             SceneManager.LoadScene("OpenQ");
         }
