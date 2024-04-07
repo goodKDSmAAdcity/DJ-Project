8ce4b806069a3e68b89c2acf9d9fc38a320d4e7f
goodKDSmAAdcity <128930159+goodKDSmAAdcity@users.noreply.github.com>
2024-04-07 03:49:14 +0300

    All

--- Assets/BossScripts/BossHP.cs
+++ Assets/BossScripts/BossHP.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossHP : MonoBehaviour
 {
@@ -30,5 +31,6 @@ public class BossHP : MonoBehaviour
     }
     void Die()
     {
+        SceneManager.LoadScene("Boss Fight Race");
     }
 }
--- Assets/InteractScript.cs
+++ Assets/InteractScript.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class InteractScript : MonoBehaviour
 {
+    public GameObject boss;
+    public GameObject deathEffect;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Finish"))
@@ -20,6 +22,19 @@ public class InteractScript : MonoBehaviour
         if (collision.gameObject.CompareTag("FinalBoss"))
         {
             SceneManager.LoadScene("BOSSFIGHT");
+        }
+        if (collision.gameObject.CompareTag("Shop")){
+            SceneManager.LoadScene("Vendor");
+        }
+        if (collision.gameObject.CompareTag("FinalBossLine"))
+        {
+            SceneManager.LoadScene("MainLobby"); 
+        }
+        if (collision.gameObject.CompareTag("KillBoss"))
+        {
+            GameObject death = Instantiate(deathEffect, boss.transform.position, boss.transform.rotation);
+            Destroy(death, death.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+            Destroy(boss);
         }
     }
 }
