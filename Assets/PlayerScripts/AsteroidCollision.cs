ceaaaec08121b0e1d04f8c4cd7f78237279331ef
goodKDSmAAdcity <128930159+goodKDSmAAdcity@users.noreply.github.com>
2025-01-16 11:50:54 +0200

    added lobby

--- Assets/PlayerScripts/AsteroidCollision.cs
+++ Assets/PlayerScripts/AsteroidCollision.cs
@@ -4,10 +4,13 @@ using System.Collections;
 
 public class AsteroidCollision : MonoBehaviour
 {
-   public float restartDelay = 0f;
+    public float restartDelay = 2f; // Delay before restarting the level
     public GameObject deathEffect;
-    public int sceneIndex;
-
+    public int sceneIndex; // Optional: Set this if you want to load a specific scene
+    public GameObject sprite;
+    public Rigidbody2D rb;
+    public Follow followscript;
+    public Weapon Weaponscript;
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Asteroid"))
@@ -18,24 +21,26 @@ public class AsteroidCollision : MonoBehaviour
 
     public void DestroyShip()
     {
-
+        // Create a death effect at the player's position
         GameObject death = Instantiate(deathEffect, transform.position, transform.rotation);
-        Destroy(death, death.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
-        Destroy(gameObject);
-        SceneManager.LoadScene(sceneIndex);
 
-        //Invoke("RestartLevel", restartDelay);
+        // Destroy the death effect after its animation finishes
+        Destroy(death, 2f); // Adjust time if the animation length is different
+
+        // Destroy the player game object
+        Destroy(sprite);
+        Destroy(rb);
+        followscript.enabled = false;
+        Weaponscript.enabled = false;
+        // Restart the level after a delay
+        Invoke("RestartLevel", restartDelay);
     }
 
     public void RestartLevel()
-   {
+    {
         // Get the current scene index
-        //int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 
-        // Load the current scene again after a delay
-        //SceneManager.LoadScene(currentSceneIndex);
-        //Application.LoadLevel (Application.loadedLevel);
-
-
-  }
+        // Reload the current scene
+        SceneManager.LoadScene(sceneIndex);
+    }
 }
