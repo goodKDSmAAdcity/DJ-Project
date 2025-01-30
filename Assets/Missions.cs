2d2a26aa450e695e21df799c7e11ff3c1f18f27c
goodKDSmAAdcity <128930159+goodKDSmAAdcity@users.noreply.github.com>
2025-01-30 11:58:22 +0200

    hahaha
    aptaptapt

--- Assets/Missions.cs
+++ Assets/Missions.cs
@@ -1,14 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Missions : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    public SceneCheck scene;
+    private int sceneIndex;
+    public bool r1 = false;
+    public bool r2 = false;
+    public bool r3 = false;
+    public bool c1 = false;
+    public bool c2 = false;
+    public bool c3 = false;
+    public bool bossunlock= false;
+
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.CompareTag("Player"))
+        if (collision.CompareTag("FinishLine"))
         {
-            Debug.Log("Player entered the trigger!");
+            if(sceneIndex==1) 
+            {
+                r1 = true;
+                SceneManager.LoadScene(11); // 11 lobby
+            }
+            if(sceneIndex==2)
+            {
+                r2 = true;
+                SceneManager.LoadScene(11);
+            }
+            if (sceneIndex == 3)
+            {
+                r3 = true;
+                SceneManager.LoadScene(11);
+            }
+            if (sceneIndex == 7)
+            {
+                SceneManager.LoadScene(8); // 8 ending
+            }
         }
     }
     void Start()
@@ -17,6 +47,6 @@ public class Missions : MonoBehaviour
     }
     void Update()
     {
-        
+        sceneIndex= scene.sceneIndex;
     }
 }
